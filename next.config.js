9ebf2feff26ce07fd7cc0f3ff32846b06b951293
dctripp-source <danijel.curlic@pm.me>
2025-05-24 14:09:21 +0200

    Fix dashboard page error

--- next.config.js
+++ next.config.js
@@ -1,12 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '**.amazonaws.com',
-      },
-    ],
+  output: 'standalone',
+  experimental: {
+    outputFileTracingRoot: undefined,
   },
 }
 
